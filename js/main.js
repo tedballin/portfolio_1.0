d3d84114d15920d28d9e8afcfdb89ac562d2f264
Yang_ <tedz@live.cn>
2018-03-24 15:34:28 -0700

    Basic Layout, Two projects, Bio, Contact

---
+++ js/main.js
@@ -0,0 +1,9 @@
+function openNav() {
+    document.getElementById("toggleNav").style.width = "100%";
+    
+}
+
+function closeNav() {
+    document.getElementById("toggleNav").style.width = "0";
+
+}
